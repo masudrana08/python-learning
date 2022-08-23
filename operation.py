54242689502c4e9dcf16f5fd9d791b0891eb81cc
masudrana08 <rana028511@gmail.com>
2022-08-23 08:34:19 +0600

    operation

---
+++ operation.py
@@ -0,0 +1,23 @@
+a = 15 / 2
+print(a)
+
+# Floor division, Division then floor
+b = 15//2
+print(b)
+
+x = 10
+y = 15
+
+if x<y and y>x:
+   print('x less than y and y less than x')
+
+c = {
+   'name' : 'masud',
+   'roll': 11
+}
+d = c
+print(c is d)
+print('name' in c, 'name in c')
+
+e = ['amar', 'sonar', 'bangla']
+print('amar' not in e)
